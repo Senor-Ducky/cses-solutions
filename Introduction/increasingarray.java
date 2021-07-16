138ccc4762e841434b1a770f5a26be7f93da6d87
Supreme-Watermelon <robom460@gmail.com>
2021-07-16 12:55:29 +0530

    Increasing Array Problem

--- Introduction/increasingarray.java
+++ Introduction/increasingarray.java
@@ -1,18 +1,26 @@
 import java.util.*;
-
+ 
 public class increasingarray {
     public static void main(String[] args) {
         int n;
+        long result=0;
+        long operations=0;
         Scanner sc = new Scanner(System.in);
-
+ 
         n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        for(int i=1; i<n; i++) {
-            arr[i] = sc.nextInt();
+ 
+        long[] arr = new long[n+1];
+ 
+        for(int i=1; i<arr.length; i++) {
+            arr[i] = sc.nextLong();
             
-
+            if(arr[i] < arr[i-1]) {
+                 result= arr[i-1] - arr[i];
+                 arr[i] = result+arr[i];
+                 operations = operations+result;
+            }
+ 
         }
+        System.out.println(operations);
     }
 }
